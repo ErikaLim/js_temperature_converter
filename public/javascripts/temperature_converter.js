d9cd48d5191bd2f37a04f0dde58442a9b0c12033
John Foley <john@hisfoleyness.com>
2015-01-13 10:55:26 -0800

    initial commit

---
+++ public/javascripts/temperature_converter.js
@@ -0,0 +1,8 @@
+function convertFahrenheitToCelsius(fahrenheit) {
+  return (fahrenheit - 32) / 1.8;
+};
+
+function convertKelvinToCelsius(kelvin) {
+  console.log(kelvin - 273.15);
+  return kelvin - 273.15;
+};
