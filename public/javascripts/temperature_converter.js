ba45c3d64f98ff259c2ce0c58955c6bec48997ab
ErikaLim <Erika.megan.lim@gmail.com>
2015-01-13 12:13:53 -0800

    finished

--- public/javascripts/temperature_converter.js
+++ public/javascripts/temperature_converter.js
@@ -6,3 +6,23 @@ function convertKelvinToCelsius(kelvin) {
   console.log(kelvin - 273.15);
   return kelvin - 273.15;
 };
+
+function convertCelsiusToFahrenheit(celsius) {
+  console.log(celsius-32);
+  return (celsius - 32) * 1.8;
+};
+
+function convertCelsiusToKelvin(celsius) {
+  console.log(celsius + 273.15);
+  return celsius + 273.15;
+};
+
+function convertFahrenheitToKelvin(fahrenheit) {
+  console.log(fahrenheit + 459.67) * (5/9);
+  return (fahrenheit + 459.67) * (5/9);
+};
+
+function convertKelvinToFahrenheit(kelvin) {
+  console.log(kelvin-273.15) * 1.8 + 32;
+  return (kelvin-273.15) * 1.8 + 32;
+};
--- spec/javascripts/temperature_converter_spec.js
+++ spec/javascripts/temperature_converter_spec.js
@@ -17,3 +17,40 @@ describe('convertKelvinToCelsius', function() {
     expect(celsiusTemp).toBeCloseTo(0.85, 3);
   });
 });
+
+describe('convertCelsiusToFahrenheit', function(){
+  it('converts celsius to fahrenheit', function (){
+    var celsius = 32;
+
+    var fahrenheitTemp = convertCelsiusToFahrenheit(celsius);
+
+    expect(fahrenheitTemp).toBeCloseTo(0, 3);
+  });
+});
+
+describe('convertCelsiusToKelvin', function(){
+  it('converts celsius to kelvin', function(){
+    var celsius = 0;
+
+    var kelvinTemp = convertCelsiusToKelvin(celsius);
+    expect(kelvinTemp).toBeCloseTo(273.15, 3);
+  });
+});
+
+describe('convertFahrenheitToKelvin', function(){
+  it('converts fahrenheit to kelvin', function(){
+    var fahrenheit = 68;
+
+    var kelvinTemp = convertFahrenheitToKelvin(fahrenheit);
+    expect(kelvinTemp).toBeCloseTo(293.15, 3);
+  });
+});
+
+describe('convertKelvinToFahrenheit', function(){
+  it('converts kelvin to fahrenheit', function(){
+    var kelvin = 293.15;
+
+    var fahrenheitTemp = convertKelvinToFahrenheit(kelvin);
+    expect(fahrenheitTemp).toBeCloseTo(68, 3);
+  });
+});
